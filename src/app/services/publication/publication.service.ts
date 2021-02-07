84739310407927e0e4a09413ad02ed737b8ad5a8
EdwardLynx24 <eduardodarkar24@gmail.com>
2021-02-07 17:34:51 -0600

    nuevas cosas

--- src/app/services/publication/publication.service.ts
+++ src/app/services/publication/publication.service.ts
@@ -8,30 +8,32 @@ import { Injectable } from '@angular/core';
 })
 export class PublicationService {
   path = environment.api
-  constructor(private Http:HttpClient) { }
-  public getAllPosts(){
-    return this.Http.get<PublicationInterface[]>(this.path+'api/v1/show/publication');
+  constructor(private Http: HttpClient) { }
+  public getUserID(){
+    return this.Http.get(this.path+`api/v1/getId`)
   }
-  /*public addNewtPost(publicacion:PublicationInterface){
-    return this.Http.post(this.path+`api/v1/insert/publication`,publicacion);
-  }*/
-  public getPostTitle(publicacion:PublicationInterface){
-    return this.Http.get(environment.api+`api/v1/show/publication/title`)
+  public getAllPosts() {
+    return this.Http.get<PublicationInterface[]>(this.path + 'api/v1/show/publication');
   }
-  public addNewtPost(publicacion:PublicationInterface){
-    return this.Http.post(environment.api+`api/v1/insert/publication`,publicacion);
+  public getPostID(publicacion: PublicationInterface) {
+    return this.Http.get<PublicationInterface[]>(this.path + `api/v1/show/publication/id?id=${publicacion.id}`);
   }
-  public upTitlePost(publicacion:PublicationInterface){
-    const path = environment.api+`api/v1/update/publication/title/`
-    return this.Http.put<PublicationInterface>(path,publicacion)
+  public getPostTitle(publicacion: PublicationInterface) {
+    return this.Http.get<PublicationInterface[]>(this.path + `api/v1/show/publication/title?title=${publicacion.title}`)
   }
-  public upContentPost(publicacion:PublicationInterface){
-    const path = environment.api+`api/v1/update/publication/text/`
-    return this.Http.put<PublicationInterface>(path,publicacion)
+  public addNewtPost(publicacion: PublicationInterface) {
+    return this.Http.post(environment.api + `api/v1/insert/publication`, publicacion);
   }
-  //Help
-  public deletePost(publicacion:PublicationInterface){
-    const path = environment.api+`api/v1/delete/publication/`
+  public upTitlePost(publicacion: PublicationInterface) {
+    const path = environment.api + `api/v1/update/publication/title/`
+    return this.Http.put<PublicationInterface>(path, publicacion)
+  }
+  public upContentPost(publicacion: PublicationInterface) {
+    const path = environment.api + `api/v1/update/publication/text/`
+    return this.Http.put<PublicationInterface>(path, publicacion)
+  }
+  public deletePost(publicacion: PublicationInterface) {
+    const path = environment.api + `api/v1/delete/publication?id=${publicacion.id}`
     return this.Http.delete(path)
   }
 }
